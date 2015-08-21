49952db798961ddc37281c6dfc7b801589c42ab9
Jiun Wei Chia <jiunwei.moe@gmail.com>
2015-08-21 16:33:52 +0800

    Add flowchart parser to images folder

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,17 @@
 // jshint devel:true
-mermaid.initialize({startOnLoad:true});
+
+$(function() {
+
+  function refreshFlowchart(markup) {
+    var holder = $("#mermaid-holder");
+    holder.empty();
+    $("<div>").addClass("mermaid").text(markup).appendTo(holder);
+    mermaid.init();
+  }
+
+  $.get("../images/flowchart-factorial.txt", function(data) {
+    var result = parser.parse(data);
+    console.log(result);
+  });
+
+});
