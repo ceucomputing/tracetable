58e13472bb1056cadf9e112f2b8aa70b4834ef4b
Jiun Wei Chia <jiunwei.moe@gmail.com>
2017-01-06 07:56:25 +0800

    Add polyfill for String.endsWith()

--- app/scripts/vendor/polyfills.js
+++ app/scripts/vendor/polyfills.js
@@ -3,3 +3,17 @@
 SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function(toElement) {
     return toElement.getScreenCTM().inverse().multiply(this.getScreenCTM());
 };
+
+// Polyfill for String.endsWith()
+// Source: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.lastIndexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
